6d52746d3f6f3c984ea7a43df3b733973b278700
fabienpuissant <fabienpuissant@live.fr>
2024-12-02 21:59:54 +0100

    day two

--- packages/day_02/src/main.rs
+++
@@ -1,7 +0,0 @@
-use day_02::find_safe_reports;
-
-fn main() {
-    let safe_reports = find_safe_reports("src/resources/puzzle.txt");
-
-    println!("Total safe reports: {safe_reports}")
-}
--- packages/day_02/tests/component_test.rs
+++
@@ -1,6 +0,0 @@
-use day_02::find_safe_reports;
-
-#[test]
-fn it_should_find_safe_reports() {
-    assert_eq!(find_safe_reports("tests/resources/puzzle.txt"), 2);
-}
