cbc70139e84ced51486881515b58a07c7bc57ae4
fabienpuissant <fabienpuissant@live.fr>
2024-12-03 13:18:12 +0100

    part two compnent test

--- packages/day_03/tests/component_test.rs
+++ packages/day_03/tests/component_test.rs
@@ -1,6 +1,11 @@
-use day_03::result_uncorrupt_file;
+use day_03::{result_uncorrupt_file, result_uncorrupt_file_with_closures};
 
 #[test]
 fn it_should_uncorrupt_file() {
     assert_eq!(result_uncorrupt_file("tests/resources/puzzle.txt"), 161);
+}
+
+#[test]
+fn it_should_uncorrupt_file_with_closures() {
+    assert_eq!(result_uncorrupt_file_with_closures("tests/resources/puzzle.txt"), 48);
 }
